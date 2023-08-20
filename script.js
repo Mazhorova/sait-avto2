f9d3884c78df154cb3faecde2771913dbc9a2285
Mazhorova <mazhorova1987@mail.ru>
2023-08-20 16:13:07 +0300

    sdf

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
-const hendClick = (offset)=>{
-  const activeSlide = document.guerySelector("[date active]")
-  const slides = [...document.gueryselectorAll("slide")]
-  const currentindex = [...slides].indexOf(activeSlide)
-  let newIndex = currentindex+offset
+// const hendClick = (offset)=>{
+//   const activeSlide = document.guerySelector("[date active]")
+//   const slides = [...document.gueryselectorAll("slide")]
+//   const currentindex = [...slides].indexOf(activeSlide)
+//   let newIndex = currentindex+offset
 
-  if (newIndex < 0) newIndex = slides.lenght-1
-  if (newIndex >= slides.lenght) newIndex = 0
-  slides[newindex].dataset.active = true
-  delete activeSlide.dataset.active
+//   if (newIndex < 0) newIndex = slides.lenght-1
+//   if (newIndex >= slides.lenght) newIndex = 0
+//   slides[newindex].dataset.active = true
+//   delete activeSlide.dataset.active
+// }
+// const onNext = () => hendClick(1)
+// const onPrev = () => hendClick(-1)
+
+let offset = 0;
+const sliderLine = document.querySelector('.slider-line');
+document.querySelector('.slider-next').addEventListener('click', function(){
+  offset = offset + 450;
+  if (offset > 1800) {
+    offset = 0
+  }
+  sliderLine.style.left = -offset + "px"
 }
-const onNext = () => hendClick(1)
-const onPrev = () => hendClick(-1)
+
+)
+
+document.querySelector('.slider-prev').addEventListener('click', function(){
+  offset = offset - 450;
+  if (offset > 0) {
+    offset = 1800;
+  }
+  sliderLine.style.left = -offset + "px"
+}
+
+)
