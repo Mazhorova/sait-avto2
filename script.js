18844e0f5fad703e372eae61a984a159a0cc4ffd
Mazhorova <mazhorova1987@mail.ru>
2023-08-22 19:52:10 +0300

    hgt

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-// const hendClick = (offset)=>{
-//   const activeSlide = document.guerySelector("[date active]")
-//   const slides = [...document.gueryselectorAll("slide")]
-//   const currentindex = [...slides].indexOf(activeSlide)
-//   let newIndex = currentindex+offset
 
-//   if (newIndex < 0) newIndex = slides.lenght-1
-//   if (newIndex >= slides.lenght) newIndex = 0
-//   slides[newindex].dataset.active = true
-//   delete activeSlide.dataset.active
-// }
-// const onNext = () => hendClick(1)
-// const onPrev = () => hendClick(-1)
 
 let offset = 0;
 const sliderLine = document.querySelector('.slider-line');
@@ -26,7 +14,7 @@ document.querySelector('.slider-next').addEventListener('click', function(){
 
 document.querySelector('.slider-prev').addEventListener('click', function(){
   offset = offset - 450;
-  if (offset > 0) {
+  if (offset < 0) {
     offset = 1800;
   }
   sliderLine.style.left = -offset + "px"
